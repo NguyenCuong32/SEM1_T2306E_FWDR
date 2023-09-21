11ff0c8805f9d6c83cf5edaddb6ff855288ec602
hoangtung1987 <hoangtung1987@gmail.com>
2023-09-21 09:11:48 +0700

    user_info

---
+++ practice/userinfo/src/Footer.js
@@ -0,0 +1,15 @@
+import './footer.css';
+
+function Footer() {
+  return (
+    <footer>
+    <div class="flex-container">
+      <div class="flex-item5">Address: No 8, Ton That Thuyet, Ha Noi</div>
+      <button class="flex-item6"><a href="https://www.facebook.com/" className='footer'>Facebook</a></button>
+      <button class="flex-item7"><a href="https://zalo.me/pc" className='footer'>Zalo</a></button>
+    </div>
+  </footer>
+  );
+}
+
+export default Footer;
