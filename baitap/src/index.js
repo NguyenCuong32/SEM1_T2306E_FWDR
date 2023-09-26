045c25ceea976255a8698a2fe1c11831ca3a0e3e
a.tu <tumanth2306008@fpt.edu.vn>
2023-09-26 20:19:20 +0700

    TUMA bài tập ve nhà

--- baitap/src/index.js
+++ baitap/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Table from './table.js';
-
+import LoginForm from './Form.js';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
+    <LoginForm/>
     <Table />
+    
   </div>
 );
 
